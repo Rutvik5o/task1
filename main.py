c11e291208fe8d40d883b5bc0a5e71071e58cfc7
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2026-01-03 13:22:48 +0530

    Added files

---
+++ main.py
@@ -0,0 +1,11 @@
+from add import add
+from subtract import subtract
+
+a = 10
+b = 5
+
+result_add = add(a, b)
+result_sub = subtract(a, b)
+
+print("Addition:", result_add)
+print("Subtraction:", result_sub)
